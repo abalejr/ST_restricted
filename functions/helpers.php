796875a4e918d1414156c11a5ea6c14ed10451ca
A.J. Bale <abalejr@gmail.com>
2018-08-13 14:40:49 -0500

    Reverse order of array_intersect() args

--- functions/helpers.php
+++ functions/helpers.php
@@ -408,25 +408,25 @@ function wl_redirect_if_not_logged_in()
 
         if (is_page(401613) or $parent_page_id === 401613) {
             $valid_memberships = array("optionsgold", "optionssilver", "academy");
-            $user_valid_memberships = array_intersect($user_active_memberships, $valid_memberships);
+            $user_valid_memberships = array_intersect($valid_memberships, $user_active_memberships);
         } else if (is_page(401958) or $parent_page_id === 401958) {
             $valid_memberships = array("futuresgold", "futuressilver");
-            $user_valid_memberships = array_intersect($user_active_memberships, $valid_memberships);
+            $user_valid_memberships = array_intersect($valid_memberships, $user_active_memberships);
         } else if (is_page(401988) or $parent_page_id === 401988) {
             $valid_memberships = array("optionsgold", "futuresgold", "bias", "fibonaccigold", "crypto", "foundation");
-            $user_valid_memberships = array_intersect($user_active_memberships, $valid_memberships);
+            $user_valid_memberships = array_intersect($valid_memberships, $user_active_memberships);
         } else if (is_page(402186) or $parent_page_id === 402186) {
             $valid_memberships = array("bias");
-            $user_valid_memberships = array_intersect($user_active_memberships, $valid_memberships);
+            $user_valid_memberships = array_intersect($valid_memberships, $user_active_memberships);
         } else if (is_page(402191) or $parent_page_id === 402191) {
             $valid_memberships = array("fibonaccigold", "fibonaccisilver");
-            $user_valid_memberships = array_intersect($user_active_memberships, $valid_memberships);
+            $user_valid_memberships = array_intersect($valid_memberships, $user_active_memberships);
         } else if (is_page(402190) or $parent_page_id === 402190) {
             $valid_memberships = array("crypto", "cryptosilver");
-            $user_valid_memberships = array_intersect($user_active_memberships, $valid_memberships);
+            $user_valid_memberships = array_intersect($valid_memberships, $user_active_memberships);
         } else if (is_page(402907) or $parent_page_id === 402907) {
             $valid_memberships = array("scanner");
-            $user_valid_memberships = array_intersect($user_active_memberships, $valid_memberships);
+            $user_valid_memberships = array_intersect($valid_memberships, $user_active_memberships);
         }
 
         if (count($user_valid_memberships) < 1) {
